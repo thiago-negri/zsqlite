9f22f2354786aa2d1887ed894de99c297737c27c
Thiago Negri <evohunz@gmail.com>
2024-11-17 08:10:32 -0300

    Fix build

--- build.zig
+++ build.zig
@@ -4,36 +4,22 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const sqlite = b.dependency("sqlite", .{ .target = target, .optimize = optimize });
+    const zsqlite_mod = b.addModule("zsqlite", .{
+        .root_source_file = b.path("src/root.zig"),
+    });
 
-    const lib = b.addStaticLibrary(.{
-        .name = "zsqlite",
+    // Add SQLite C as a static library
+    const zsqlite_c = b.dependency("zsqlite-c", .{ .target = target, .optimize = optimize });
+    const zsqlite_c_artifact = zsqlite_c.artifact("zsqlite-c");
+    zsqlite_mod.linkLibrary(zsqlite_c_artifact);
+
+    const zsqlite_lib_test = b.addTest(.{
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-    lib.addIncludePath(sqlite.path(""));
-    lib.installHeader(sqlite.path("sqlite3.h"), "sqlite3.h");
-    lib.installHeader(sqlite.path("sqlite3ext.h"), "sqlite3ext.h");
-    lib.addCSourceFiles(.{
-        .root = sqlite.path(""),
-        .files = &.{"sqlite3.c"},
-    });
-    lib.linkLibC();
-    b.installArtifact(lib);
+    const zsqlite_lib_test_run = b.addRunArtifact(zsqlite_lib_test);
 
-    const module = b.addModule("zsqlite", .{
-        .root_source_file = b.path("src/root.zig"),
-    });
-    module.addIncludePath(sqlite.path(""));
-
-    const lib_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_lib_unit_tests.step);
+    const step_test = b.step("test", "Run unit tests");
+    step_test.dependOn(&zsqlite_lib_test_run.step);
 }
